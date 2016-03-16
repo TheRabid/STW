2c5f791731c965854108068318d863f6a1eb9bf1
Jaime <jai_mi_to@hotmail.es>
2016-03-16 17:32:44 +0100

    Lots of changes

--- PracticasSTW/src/p3/index.js
+++ PracticasSTW/src/p3/index.js
@@ -11,5 +11,6 @@ handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 handle["/show"] = requestHandlers.show;
+handle["/setMemo"] = requestHandlers.setMemo;
 
 server.start(router.route, handle);
--- PracticasSTW/src/p3/requestHandlers.js
+++ PracticasSTW/src/p3/requestHandlers.js
@@ -5,7 +5,7 @@ var querystring = require("querystring");
 var fs = require("fs");
 var formidable = require("formidable");
 
-function start(response) {
+function start(response, request) {
 	console.log("Request handler 'start' was called.");
 
 	var body= '<html>'+
@@ -14,6 +14,21 @@ function start(response) {
 	'content="text/html; charset=UTF-8" />'+
 	'</head>'+
 	'<body>'+
+	'<h1>' +
+	'=-=-= Memo System =-=-=' +
+	'</h1>' +
+	'<h2>' +
+	'Choose an option' +
+	'</h2>' +
+	'<form action="/setMemo">' +
+	'<input type="submit" value="Set Memo">' +
+	'</form>' +
+	'<form action="/deleteMemo">' +
+	'<input type="submit" value="Delete Memo">' +
+	'</form>' +
+	'<form action="/showAllMemo">' +
+	'<input type="submit" value="Show All Memo">' +
+	'</form>' +
 	'<form action="/upload" enctype="multipart/form-data" '+
 	'method="post">'+
 	'<input type="file" name="upload" multiple="multiple">'+
@@ -49,12 +64,45 @@ function upload(response, request) {
 		response.end(); });
 }
 
-function show(response) {
+function show(response, request) {
 	console.log("Request handler 'show' was called.");
 	response.writeHead(200, {"Content-Type": "image/png"});
 	fs.createReadStream("./tmp/test.png").pipe(response);
 }
 
+function setMemo(response, request){
+	console.log("Request handler 'setMemo' was called.")
+	var body= '<html>'+
+	'<head>'+
+	'<meta http-equiv="Content-Type" '+
+	'content="text/html; charset=UTF-8" />'+
+	'</head>'+
+	'<body>'+
+	'<h1>' +
+	'=-=-= Memo System =-=-=' +
+	'</h1>' +
+	'<h2>' +
+	'New memo' +
+	'</h2>' +
+	'<form action="/setMemo">' +
+	'Name of memo:<br>' +
+	'<input type="text" name="name" required><br>' +
+	'Description of memo:<br>' +
+	'<textarea name="description" rows="20" cols="30" required></textarea><br>' +
+	'Optional file:<br>'+
+	'<input type="file" name="file" multiple="multiple"><br><br>'+
+	'<input type="submit" value="Submit memo">' +
+	'</form>' +
+	'</body>'+
+	'</html>';
+	response.writeHead(200, {
+		"Content-Type" : "text/html"
+	});
+	response.write(body);
+	response.end();
+}
+
 exports.start = start;
 exports.upload = upload;
 exports.show = show;
+exports.setMemo = setMemo;
