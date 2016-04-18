52f3fdf286d42774038971c228f91400d77e2726
Jaime <jai_mi_to@hotmail.es>
2016-04-18 12:55:04 +0200

    Preparado todo para la p5

---
+++ PracticasSTW/src/p5/database.js
@@ -0,0 +1,312 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+/**
+ * Database.js
+ * Este fichero contiene todas las funciones de llamada a la base de datos
+ */
+
+ /**
+  * Funcion insertMemo. Se encarga de insertar en la base de datos
+  * una memo que no tenga fichero asociado. Si no hay error llama a la funcion
+  * de callback sin error asociado
+  */
+
+function insertMemo(memoName, memoDesc, memoDate, callback){
+	MongoClient.connect('mongodb://127.0.0.1:27017/memoSystem',function(err,connection){
+			if (err) {
+				console.log(err);
+				callback(err);
+			}
+
+      var collection = connection.collection('memos');
+
+			console.log('--Connected to database');
+
+      collection.insert({'MemoName':memoName,'MemoDesc':memoDesc,'MemoDate':memoDate},
+        function(err, count){
+					connection.close();
+					if(!err) {
+						 console.log("--Insertion complete!");
+						 callback("");
+					} else{
+						console.log("--Insertion failure");
+						console.log(err);
+						callback(err);
+					}
+			});
+		});
+}
+
+/**
+ * Funcion insertMemoComplete. Se encarga de insertar en la base de datos
+ * una memo que tenga fichero asociado. Si no hay error llama a la funcion
+ * de callback sin error asociado
+ */
+
+function insertMemoComplete(memoName, memoDesc, memoDate, memoFile,
+            memoFileName, callback){
+  MongoClient.connect('mongodb://127.0.0.1:27017/memoSystem',function(err,connection){
+  		if (err) {
+  			console.log(err);
+  			callback(err);
+  		}
+
+      var collection = connection.collection('memos');
+
+  		console.log('--Connected to database');
+
+			collection.insert(
+        {'MemoName':memoName,'MemoDesc':memoDesc,'MemoDate':memoDate,
+        'MemoFile':memoFile,'MemoFileName':memoFileName},
+        function(err,count){
+					connection.close();
+					if(!err) {
+						 console.log("--Insertion complete!");
+						 callback("");
+					} else{
+						console.log("--Insertion failure");
+						console.log(err);
+						callback(err);
+					}
+			});
+		});
+}
+
+/**
+ * Funcion getAllMemo. Se encarga de devolver todas las memos de la base de
+ * datos. Si no hay error llama a la funcion de callback sin error asociado y
+ * con las memos encontradas
+ */
+
+function getAllMemo(callback){
+  MongoClient.connect('mongodb://127.0.0.1:27017/memoSystem',function(err,connection){
+			if (err) {
+				console.log(err);
+				callback(err,null);
+			}
+
+      var collection = connection.collection('memos');
+
+			console.log('--Connected to database');
+
+			collection.find().toArray(function(err,documents){
+					connection.close();
+          console.log(documents);
+					if(!err) {
+						 console.log("--Obtained all memos!");
+             var rows = documents;
+						 callback(null, rows);
+					} else{
+						console.log("--Failed to get all memos");
+						console.log(err);
+						callback(err,null);
+					}
+			});
+		});
+}
+
+/**
+ * Funcion getAllMemo. Se encarga de devolver la memo referenciada de la base de
+ * datos. Si no hay error llama a la funcion de callback sin error asociado y
+ * con la memo en cuestion
+ */
+
+function getMemo(idMemo, callback){
+  MongoClient.connect('mongodb://127.0.0.1:27017/memoSystem',function(err,connection){
+			if (err) {
+				console.log(err);
+				callback(err,null);
+			}
+
+      var collection = connection.collection('memos');
+			console.log('--Connected to database');
+			collection.find(ObjectId(idMemo)).toArray(function(err,documents){
+        console.log(documents);
+        connection.close();
+					if(!err) {
+            if(documents.length > 0){
+              console.log("--Obtained the memo!");
+              callback(null, documents);
+            } else{
+              console.log("--Memo does not exist!");
+              callback(null,null);
+            }
+					} else{
+						console.log("--Failed to get the memo");
+						console.log(err);
+						callback(err,null);
+					}
+			});
+		});
+}
+
+/**
+ * Funcion dropMemo. Se encarga de eliminar la memo referenciada de la base de
+ * datos. Si no hay error llama a la funcion de callback sin error asociado y
+ * con el numero de memos afectadas
+ */
+
+function dropMemo(idMemo, callback){
+  MongoClient.connect('mongodb://127.0.0.1:27017/memoSystem',function(err,connection){
+			if (err) {
+				console.log(err);
+				callback(err);
+			}
+
+			var collection = connection.collection('memos');
+			console.log('--Connected to database');
+
+			collection.remove({"_id":ObjectId(idMemo)},true,function(err,documents){
+					connection.close();
+					console.log(documents);
+					if(documents > 0) {
+            console.log("--Deleted the memo " + idMemo + " succesfully!");
+            callback(null);
+					} else{
+						console.log("--Failed to delete the memo " + idMemo);
+						console.log("--No affected rows");
+						callback(documents);
+					}
+			});
+		});
+}
+
+function blobMemo(idMemo, callback){
+  MongoClient.connect('mongodb://127.0.0.1:27017/memoSystem',function(err,connection){
+			if (err) {
+				console.log(err);
+				callback(err,null, null);
+			}
+
+			var collection = connection.collection('memos');
+			console.log('--Connected to database');
+
+			if(idMemo!=null){
+
+				collection.find(ObjectId(idMemo)).toArray(function(err, rows){
+						connection.close();
+						if(!err) {
+	            if(rows.length > 0 && rows[0] != null){
+	              var blob = rows[0].MemoFile;
+	              var name = rows[0].MemoFileName;
+	              console.log("--Obtained the memo!");
+	              callback(null, blob, name);
+	            } else{
+	              console.log("--Memo does not exist!");
+	              callback(null,null, null);
+	            }
+						} else{
+							console.log("--Failed to get the memo");
+							console.log(err);
+							callback(err,null);
+						}
+				});
+			} else{
+				console.log("--Failed to get the memo");
+				console.log(err);
+				callback(err,null);
+			}
+		});
+}
+
+/**
+ * Funcion loginUser
+ */
+
+function loginUser(user, pass, callback){
+ MongoClient.connect('mongodb://127.0.0.1:27017/usersMemoSystem',function(err,connection){
+		 if (err) {
+			 console.log(err);
+			 callback(err,null,null);
+		 }
+
+		 var collection = connection.collection('users');
+
+		 console.log('--Connected to database');
+
+		 collection.find({"user":""+user}).toArray(function(err, documents){
+				 if(!err) {
+					 // The user exists
+					 if(documents.length > 0){
+						 var userPass = documents[0].pass;
+						 // Password matches
+						 if(userPass === pass){
+							 console.log("--Login succesfull!");
+							 connection.close();
+							 callback(null,true,documents[0]._id);
+						 }
+						 // Password doesn't match
+						 else{
+							 console.log("--Password doesn't match");
+							 connection.close();
+							 callback(null,false,null);
+						 }
+				 	 } else{
+					 	console.log("--User doesn't exist! Auto-inserting!");
+					 	collection.insert({'user':user,'pass':pass},function(err, count){
+						 connection.close();
+						 if(!err) {
+							 console.log("--User insertion complete!");
+							 callback(null,true,count[0]._id);
+						 } else{
+							 console.log("--User insertion failure!");
+							 console.log(err);
+							 callback(err,false,null);
+						 }
+						});
+				 	}
+				} else{
+					console.log("--User insertion failure!");
+					console.log(err);
+					callback(err,false,null);
+				}
+		 });
+	 });
+}
+
+/**
+ * Funcion checkLogin
+ */
+
+function checkLogin(id, callback){
+ MongoClient.connect('mongodb://127.0.0.1:27017/usersMemoSystem',function(err,connection){
+		 if (err) {
+			 console.log(err);
+			 callback(err);
+		 }
+
+		 var collection = connection.collection('users');
+
+		 console.log('--Connected to database');
+		 collection.find(ObjectId(id)).toArray(function(err, documents){
+				 if(!err) {
+					// The user exists
+					if(documents.length > 0){
+						connection.close();
+						console.log("--Check login successfull");
+						callback(null,true);
+					}
+					else{
+						console.log("--Check login unsuccessfull");
+						console.log(user);
+						callback(null,false);
+						}
+				}
+				else{
+					console.log("--Error! ");
+					connection.close();
+					callback(err,false);
+				}
+		 });
+	 });
+}
+
+exports.insertMemo = insertMemo;
+exports.insertMemoComplete = insertMemoComplete;
+exports.getAllMemo = getAllMemo;
+exports.getMemo = getMemo;
+exports.dropMemo = dropMemo;
+exports.blobMemo = blobMemo;
+exports.loginUser = loginUser;
+exports.checkLogin = checkLogin;
---
+++ PracticasSTW/src/p5/utils.js
@@ -0,0 +1,223 @@
+/**
+ * Utils.js
+ * Este fichero contiene todas las generaciones de paginas html estaticas
+ * para facilitar su uso en el fichero requestHandlers
+ */
+
+var head = '<head>'+
+'<meta http-equiv="Content-Type" '+
+'content="text/html; charset=UTF-8" />'+
+'</head>';
+
+var headTest = '<head>'+
+'<meta http-equiv="Content-Type" '+
+'content="application/octet-stream; charset=UTF-8" />'+
+'</head>';
+
+var headTable = '<head>'+
+'<meta http-equiv="Content-Type" '+
+'content="text/html; charset=UTF-8" />'+
+'</head>' +
+'<style>' +
+'table, th, td {' +
+'border: 1px solid black;' +
+'border-collapse: collapse;' +
+'}' +
+'th, td {' +
+'padding: 5px;' +
+'text-align: center;' +
+'}</style>';
+
+var mainHeader = '<h1>' +
+'=-=-= Memo System =-=-=' +
+'</h1>';
+
+var backButton = '<form action="/start">' +
+'<input type="submit" value="Back to Main Menu">' +
+'</form><br>' +
+'<form action="/logout">' +
+'<input type="submit" value="Logout">' +
+'</form>';
+
+var logoutButton =
+'<br><form action="/logout">' +
+'<input type="submit" value="Logout">' +
+'</form>';
+
+var startBody = '<h2>' +
+'Choose an option' +
+'</h2>' +
+'<form action="/setMemo">' +
+'<input type="submit" value="Set Memo">' +
+'</form>' +
+'<form action="/deleteMemo">' +
+'<input type="submit" value="Delete Memo">' +
+'</form>' +
+'<form action="/showAllMemo">' +
+'<input type="submit" value="Show All Memo">' +
+'</form>' + logoutButton;
+
+var setMemoBody = '<h2>' +
+'New memo' +
+'</h2>' +
+'<form action="/submitMemo" enctype="multipart/form-data" method="post">' +
+'Name of memo:<br>' +
+'<input type="text" name="memoName" required><br>' +
+'Description of memo:<br>' +
+'<textarea name="memoDesc" rows="20" cols="30" required></textarea><br>' +
+'Date of memo:<br>' +
+'<input type="date" name="memoDate"><br>' +
+'Optional file:<br>'+
+'<input type="file" name="memoFile" multiple="multiple"><br><br>'+
+'<input type="submit" value="Submit memo">' +
+'</form>';
+
+var insertMemoFailed = '<h2>' +
+'Memo not created' +
+'</h2>' +
+'<h3>' +
+'The memo could not be inserted into the database' +
+'</h3>' +
+'Please try again' +
+'<br><br>' +
+'<form action="/start">' +
+'<input type="submit" value="Back to Main Menu">' +
+'</form>' +
+'</body>'+
+'</html>';
+
+var showAllBody = '<h2>' +
+'Memo List' +
+'</h2>';
+
+var showAllBodyToDelete = '<h2>' +
+'Choose a memo to delete' +
+'</h2>';
+
+var buttonList = '<br><form action="/showAllMemo">' +
+'<input type="submit" value="Back to memo list">' +
+'</form>';
+
+var buttonDelete = '<br><form action="/deleteMemo">' +
+'<input type="submit" value="Back to memo list">' +
+'</form>';
+
+var buttonAndEnd = '<br><form action="/start">' +
+'<input type="submit" value="Back to Main Menu">' +
+'</form>' +
+'</body>'+
+'</html>';
+
+var showMemoFailed = '<h2>' +
+'Memo not found' +
+'</h2>' +
+'<h3>' +
+'The memo does not exist in the database or either you have a connection error ' +
+'</h3>' +
+'Please try again' +
+'<br>' + buttonList +
+'<br><form action="/start">' +
+'<input type="submit" value="Back to Main Menu">' +
+'</form>' +
+'</body>'+
+'</html>';
+
+var showAllMemoFailed = '<h2>' +
+'Error accessing the database' +
+'</h2>' +
+'<h3>' +
+'Maybe there is a connection error ' +
+'</h3>' +
+'Please try again' +
+'<br>' + buttonList +
+'<br><form action="/start">' +
+'<input type="submit" value="Back to Main Menu">' +
+'</form>' +
+'</body>'+
+'</html>';
+
+var removeMemo = '<h2>' +
+'Memo deleted successfully' +
+'</h2>' + buttonDelete +
+'<br><form action="/start">' +
+'<input type="submit" value="Back to Main Menu">' +
+'</form>' +
+'</body>'+
+'</html>';
+
+var removeMemoFailed = '<h2>' +
+'Memo not deleted' +
+'</h2><h3>There was an error while deleting the memo. Please try again</h3>' +
+buttonDelete + '<br><form action="/start">' +
+'<input type="submit" value="Back to Main Menu">' +
+'</form>' +
+'</body>'+
+'</html>';
+
+var loginBody = '<h2>' +
+'Login' +
+'</h2>' +
+'<form action="/login" enctype="multipart/form-data" method="post">' +
+'User:<br>' +
+'<input type="text" name="user" required><br>' +
+'Password:<br>' +
+'<input type="password" name="pass"><br>' + '<br>'+
+'<input type="submit" value="Login" required>' +
+'</form>';
+
+var loginBodyFailed = '<h2>' +
+'Login' +
+'</h2>' + '<h3> Login failed. Incorrect password </h3>' +
+'<form action="/login" enctype="multipart/form-data" method="post">' +
+'User:<br>' +
+'<input type="text" name="user" required><br>' +
+'Password:<br>' +
+'<input type="password" name="pass"><br>' + '<br>'+
+'<input type="submit" value="Login" required>' +
+'</form>';
+
+/*
+ * Funcion generateResponse. En base al tipo pasado por parametro devuelve
+ * un String u otro para generar el Html
+ */
+function generateResponse(kind) {
+  var returned;
+  if(kind === "logged"){
+    returned = '<html>' + head + '<body>' + mainHeader + startBody +
+    '</body></html>';
+  } else if (kind === "login") {
+    returned = '<html>' + head + '<body>' + mainHeader + loginBody +
+    '</body></html>';
+  } else if (kind === "loginFailed") {
+    returned = '<html>' + head + '<body>' + mainHeader + loginBodyFailed +
+    '</body></html>';
+  } else if (kind === "setMemo") {
+    returned = '<html>' + head + '<body>' + mainHeader + setMemoBody +
+    backButton + '</body></html>';
+  } else if(kind === "insertMemo"){
+    returned = '<html>' + head + '<body>' + mainHeader;
+  } else if(kind === "insertMemoFailed"){
+    returned = insertMemoFailed;
+  } else if(kind === "buttonAndEnd"){
+    returned = buttonAndEnd + logoutButton;
+  } else if(kind === "showAllMemo"){
+    returned = '<html>' + headTable + '<body>' + mainHeader + showAllBody;
+  } else if(kind === "showMemo"){
+    returned = '<html>' + head + '<body>' + mainHeader;
+  } else if(kind === "showMemoFailed"){
+    returned = '<html>' + head + '<body>' + mainHeader + showMemoFailed;
+  } else if(kind === "listButtonAndEnd"){
+    returned = buttonList + buttonAndEnd + logoutButton;
+  } else if(kind === "showAllMemoToDelete"){
+    returned = '<html>' + headTable + '<body>' + mainHeader + showAllBodyToDelete;
+  }else if(kind === "removeMemo"){
+    returned = '<html>' + head + '<body>' + mainHeader + removeMemo;
+  } else if(kind === "removeMemoFailed"){
+    returned = '<html>' + head + '<body>' + mainHeader + removeMemoFailed;
+  } else if(kind === "showAllMemoFailed"){
+    returned = '<html>' + head + '<body>' + mainHeader + showAllMemoFailed;
+  }
+  return returned;
+}
+
+exports.generateResponse = generateResponse;
